c08e2bbbfa98afea016ef2821096c25df2af7fa0
Anikhet Mulky <am9559@g.rit.edu>
2024-08-26 16:09:18 -0400

    first commit

---
+++ src/lib/Hero/Scene.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { Canvas } from "@threlte/core";
+  import Shapes from "./Shapes.svelte";
+  </script>
+  
+
+  <div id="canvas-container">
+    
+      <Canvas shadows>
+        
+          <Shapes/>
+      </Canvas>
+  </div>
+  
+  <style>
+    #canvas-container {
+      width: 100vw;  /* Full width of the viewport */
+      height: 70vh; /* Full height of the viewport */
+      
+   
+    }
+    
+
+  </style>
+  
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+
+import Scene from "../lib/Hero/Scene.svelte"
+
+</script>
+<div>
+<Scene/>
+</div>

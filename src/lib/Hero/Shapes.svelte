638a7460627630f9cd4170a7e25cd12fc1611f4e
Anikhet Mulky <am9559@g.rit.edu>
2024-08-28 21:54:29 -0400

    Added interactivity and color

--- src/lib/Hero/Shapes.svelte
+++ src/lib/Hero/Shapes.svelte
@@ -1,69 +1,68 @@
 <script lang="ts">
-
-    import {T as Threlte} from "@threlte/core";
-    import * as THREE from "three";
+  import { T as Threlte } from "@threlte/core";
+  import * as THREE from "three";
   import Geometry from "./Geometry.svelte";
-  import { ContactShadows } from "@threlte/extras";
+  import {
+    ContactShadows,
+    Environment,
+    interactivity,
+    transitions,
+  } from "@threlte/extras";
+
+  interactivity();
 </script>
 
-<Threlte.PerspectiveCamera 
-makeDefault
-position={[0,0,20]}
-aspect={window.innerWidth / window.innerHeight} 
-fov={50} 
-near={1}
-far={40}
-
-
-
+<Threlte.PerspectiveCamera
+  makeDefault
+  position={[0, 0, 20]}
+  aspect={window.innerWidth / window.innerHeight}
+  fov={50}
+  near={1}
+  far={40}
 />
 
-
-<Threlte.DirectionalLight 
+<!-- <Threlte.DirectionalLight 
   position={[10, 10, 40]} 
-  intensity={5} 
+  intensity={2} 
   castShadow 
-/>
+/> -->
 
-<ContactShadows position={[0,-3.5,0]}
-opacity={0.65}
-scale={40}
-blur={2}
-far={9}></ContactShadows>
+<Environment files="smallroom.hdr" path="/" format="hdr" />
 
-<Geometry
-rate={0.3}
-position={[0,0,0]}
-geometry={new THREE.IcosahedronGeometry(3)}
-
-/>
+<ContactShadows
+  position={[0, -3.5, 0]}
+  opacity={0.65}
+  scale={40}
+  blur={2}
+  far={9}
+></ContactShadows>
 
 <Geometry
-rate={0.4}
-position={[1,-0.5,5]}
-geometry={new THREE.CapsuleGeometry(0.5,1.6,2,16)}
-
+  rate={0.3}
+  position={[0, 0, 0]}
+  geometry={new THREE.IcosahedronGeometry(3)}
 />
 
 <Geometry
-rate={0.4}
-position={[-1.5,-0.75,2]}
-geometry={new THREE.BoxGeometry(2, 2, 2, 2, 2, 2)}
-
+  rate={0.4}
+  position={[1, -0.5, 5]}
+  geometry={new THREE.CapsuleGeometry(0.5, 1.6, 2, 16)}
 />
 
-
 <Geometry
-rate={0.4}
-position={[-1.1,0.75,3]}
-geometry={new THREE.TorusGeometry( 1, 0.4, 3, 90 )}
-
+  rate={0.4}
+  position={[-1.5, -0.75, 2]}
+  geometry={new THREE.CylinderGeometry(1, 1, 3, 550)}
 />
 
+<Geometry
+  rate={0.4}
+  position={[-1.1, 0.75, 3]}
+  geometry={new THREE.TorusGeometry(1, 0.4, 3, 90)}
+/>
 
 <Geometry
-rate={0.4}
-position={[1.1,0.75,4]}
-geometry={new THREE.SphereGeometry( 1, 12, 20 )}
-
+  rate={0.4}
+  position={[1.1, 0.75, 4]}
+  geometry={new THREE.SphereGeometry(1, 12, 20)}
 />
